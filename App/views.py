a0e03e42150d4256d9cb2950dde01de18dd46b77
Jeondanin <39483767+Jeondanin@users.noreply.github.com>
2021-11-25 23:53:10 +0900

    간략하게 수정

--- App/views.py
+++ App/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
+def index(request):
+    context = {
+        'message' : '은비는 소중해',
+        'content' : '공부하자 모두들',
+    }
+    return render("index.html", context)
+
 def Register(request):
     print(request)
     return JsonResponse({'success':"ok"})
--- Setting/urls.py
+++ Setting/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from App import views
 
 urlpatterns = [
+    path('', views.index),
     path('api/', include('App.urls'))
 ]
